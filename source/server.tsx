1e3927dc2a22fd6d107886acf3cf7e22d885d78c
thuuhh <tutuadonai@gmail.com>
2023-02-19 01:04:53 -0300

    Nothing changed.

--- source/server.tsx
+++ source/server.tsx
@@ -4,6 +4,7 @@ export default function Server(){
   return (
     <div className="server">
       <h1>Lavender Core</h1>
+      <h2>The most powerful engine to create Minecraft interactive experiences.</h2>
     </div>
   )
 }
